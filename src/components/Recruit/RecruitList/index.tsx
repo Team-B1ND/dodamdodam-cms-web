b404d7e62cb5481865da5698e692be65fcbd778e
임동현 <79711744+ldh3907@users.noreply.github.com>
2023-06-27 15:23:11 +0900

    ⚡️ ::선생님 요구사항 반영

--- src/components/Recruit/RecruitList/index.tsx
+++ src/components/Recruit/RecruitList/index.tsx
@@ -31,6 +31,7 @@ const RecruitList = () => {
       <S.Container>
         {data?.data
           .filter((recruit) => recruit.createdDate.slice(0, 4) === selectedYear)
+          .reverse()
           .map((item) => (
             <RecruitItem {...item}>{item}</RecruitItem>
           ))}
